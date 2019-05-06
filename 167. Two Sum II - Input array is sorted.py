e3b6126b0e304afa8df1a42c7fdda9117f59a765
awuuuuu <asasopo@126.com>
2019-05-06 23:00:37 +0200

    Update 167. Two Sum II - Input array is sorted.py

--- 167. Two Sum II - Input array is sorted.py
+++ 167. Two Sum II - Input array is sorted.py
@@ -1,3 +1,4 @@
+# TODO
 #非一遍过，判断key是否存在不能直接在object中查
 
 class Solution(object):
@@ -14,3 +15,43 @@ class Solution(object):
             else:
                 return [m[target - n], i+1]
         return []
+
+    #已排序数组和未排序数组的区别在于，两根指针相对而行
+    class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        if len(numbers) <= 1:
+            return []
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            if numbers[j] + numbers[i] > target:
+                j = j - 1
+            elif numbers[j] + numbers[i] < target:
+                i = i + 1
+            else:
+                return [i+1, j+1]
+        return []
+    
+    优化1
+    class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        j = len(numbers) - 1
+        while i < j:
+            #必定存在一解
+            if numbers[j] + numbers[i] == target:
+                return [i+1, j+1]
+            elif numbers[j] + numbers[i] < target:
+                i = i + 1
+            else:
+                j = j - 1
