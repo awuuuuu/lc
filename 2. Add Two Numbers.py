c4d2aab52b08aca0ddaa74e64fd40464026f6944
awuuuuu <asasopo@126.com>
2019-05-07 14:12:18 +0200

    Update and rename t to 2. Add Two Numbers.py

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
@@ -14,20 +8,20 @@ class Solution(object):
         if l1 is None or l2 is None:
             return l1 if l1 else l2
         res = ListNode(0)
-        cur = res.next
+        cur = res
+        #cur 如果直接赋值为res.next则是None没有指针指向
         count = 0
         while l1 or l2 or count:
             count += (l1.val if l1 else 0) + (l2.val if l2 else 0)
-            cur = ListNode(count%10)
+            cur.next = ListNode(count%10)
             cur = cur.next
-            count = count - count%10
+            #这里的count计算要注意，不空空想
+            count = 1 if count>9 else 0
             if l1:
                 l1 = l1.next
             if l2:
                 l2 = l2.next
             if (l1 is None or l2 is None) and count == 0:
-                cur = l2 if l2 else l1
+                cur.next = l2 if l2 else l1
                 break
-        return res.next
-                
-            
+        return res.next       
