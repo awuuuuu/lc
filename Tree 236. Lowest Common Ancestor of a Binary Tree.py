73a39ef621f4477fbda4e949401b97aefa0ab12d
awuuuuu <asasopo@126.com>
2019-05-13 22:24:14 +0200

    Create Tree 236. Lowest Common Ancestor of a Binary Tree.py

---
+++ Tree 236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,12 @@
+#该用递归的时候，还是不能铁着头不用
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root is None or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        if left or right:
+            return left or right
